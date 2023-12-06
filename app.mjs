3e01084614acc0945bfa5616e32f87dc2b2ff4d5
MivanBing <ivanjing283@gmail.com>
2023-12-05 22:58:31 -0500

    fix port

--- app.mjs
+++ app.mjs
@@ -141,7 +141,7 @@ app.post('/register', async (req, res) => {
     
 });
 
-app.listen(process.env.PORT ?? 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port ${process.env.PORT ?? 3000}`);
 });
 
